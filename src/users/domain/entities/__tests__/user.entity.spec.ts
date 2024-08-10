72aca8d48969d16fc37ef53d24da7fd1bf14f3d7
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-08-10 01:12:30 -0300

    test: adds tests for user

---
+++ src/users/domain/entities/__tests__/user.entity.spec.ts
@@ -0,0 +1,24 @@
+import { UserEntity, UserProps } from '@/users/domain/entities/user.entity';
+import { faker } from '@faker-js/faker';
+
+describe('UserEntity', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    sut = new UserEntity(props);
+  });
+
+  it('should assert constructor method', () => {
+    expect(sut.props.name).toEqual(props.name);
+    expect(sut.props.email).toEqual(props.email);
+    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
