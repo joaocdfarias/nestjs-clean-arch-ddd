59b29a527c1bd615948bd192e2c20edb07e62ce1
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-08-10 14:13:00 -0300

    feat: defines getters for user entity

--- src/users/domain/entities/__tests__/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/user.entity.spec.ts
@@ -21,4 +21,27 @@ describe('UserEntity', () => {
     expect(sut.props.password).toEqual(props.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('should assert getter of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toBe(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('should assert getter of email field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toBe(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('should assert getter of password field', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toBe(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('should assert getter of createdAt field', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
 });
