3dc176f3634e81d271c42fbdbf06164d65a23621
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-08-09 22:04:34 -0300

    chore: adds env-config implementation

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,16 @@
+import { EnvConfig } from '@/shared/infrastructure/env-config/env-config.interface';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {}
+
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'));
+  }
+
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV');
+  }
+}
