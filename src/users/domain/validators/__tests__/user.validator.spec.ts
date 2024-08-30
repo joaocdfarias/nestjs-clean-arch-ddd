5cca79d70ddfe54fa8ed1dae67c2e3b5f245812a
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-08-30 18:26:09 -0300

    test: adds tests for UserValidator

---
+++ src/users/domain/validators/__tests__/user.validator.spec.ts
@@ -0,0 +1,53 @@
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data.builder';
+import {
+  UserRules,
+  UserValidator,
+  UserValidatorFactory,
+} from '@/users/domain/validators/user.validator';
+
+let sut: UserValidator;
+
+describe('UserValidator', () => {
+  beforeEach(() => {
+    sut = UserValidatorFactory.create();
+  });
+
+  describe('name', () => {
+    it('should invalidate', () => {
+      let isValid = sut.validate(null as any);
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['name']).toStrictEqual([
+        'name should not be empty',
+        'name must be a string',
+        'name must be shorter than or equal to 255 characters',
+      ]);
+
+      isValid = sut.validate({ ...UserDataBuilder({}), name: '' as any });
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['name']).toStrictEqual(['name should not be empty']);
+
+      isValid = sut.validate({ ...UserDataBuilder({}), name: 1 as any });
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['name']).toStrictEqual([
+        'name must be a string',
+        'name must be shorter than or equal to 255 characters',
+      ]);
+
+      isValid = sut.validate({
+        ...UserDataBuilder({}),
+        name: 'A'.repeat(256) as any,
+      });
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['name']).toStrictEqual([
+        'name must be shorter than or equal to 255 characters',
+      ]);
+    });
+
+    it('should validate', () => {
+      const props = UserDataBuilder({});
+      const isValid = sut.validate(props);
+      expect(isValid).toBeTruthy();
+      expect(sut.validatedData).toStrictEqual(new UserRules(props));
+    });
+  });
+});
