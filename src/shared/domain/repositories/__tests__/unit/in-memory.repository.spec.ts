f10a2e3e23c38ba39c14d1e6bbfae140579eb5a7
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2025-03-04 04:04:17 -0300

    test: adds test for findAll method

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -40,4 +40,13 @@ describe('InMemoryRepository unit tests', () => {
 
     expect(entity.toJSON()).toStrictEqual(result.toJSON());
   });
+
+  it('should return all entities', async () => {
+    const entity = new StubEntity({ name: 'Test', price: 50 });
+
+    await sut.insert(entity);
+    const result = await sut.findAll();
+
+    expect([entity]).toStrictEqual(result);
+  });
 });
