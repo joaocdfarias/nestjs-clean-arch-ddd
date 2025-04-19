ca71bef1d08898aac8c61b2fa2f09737fdae6f0c
João Farias <joaocdfariass@gmail.com>
2025-04-18 21:41:05 -0300

    test: creates tests for sort, sortDirection and filter

--- src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
@@ -54,5 +54,94 @@ describe('Searchable Repository', () => {
         expect(sut.perPage).toBe(expected);
       });
     });
+
+    describe('sort', () => {
+      it('should return page 1 sorted', () => {
+        const sut = new SearchParams();
+
+        expect(sut.sort).toBe(null);
+      });
+
+      it.each([
+        [null, null],
+        [undefined, null],
+        ['', null],
+        ['test', 'test'],
+        [0, '0'],
+        [-1, '-1'],
+        [5.5, '5.5'],
+        [true, 'true'],
+        [false, 'false'],
+        [{}, '[object Object]'],
+        [1, '1'],
+        [2, '2'],
+        [25, '25'],
+      ])('sorted param %o should return %o', (sort, expected) => {
+        const sut = new SearchParams({ sort: sort as any });
+        expect(sut.sort).toBe(expected);
+      });
+    });
+
+    describe('sortDirection', () => {
+      it('should return null if no value is provided', () => {
+        const sut = new SearchParams();
+
+        expect(sut.sortDirection).toBeNull();
+      });
+
+      it('should return null if sort is null', () => {
+        const sut = new SearchParams({ sort: null });
+
+        expect(sut.sortDirection).toBeNull();
+      });
+
+      it.each([
+        [null, 'DESC'],
+        [undefined, 'DESC'],
+        ['', 'DESC'],
+        ['test', 'DESC'],
+        [0, 'DESC'],
+        [{}, 'DESC'],
+        ['ASC', 'ASC'],
+        ['DESC', 'DESC'],
+      ])(
+        'param %o should return sort direction %o',
+        (sortDirection, expected) => {
+          const sut = new SearchParams({
+            sort: 'test',
+            sortDirection: sortDirection as any,
+          });
+
+          expect(sut.sortDirection).toBe(expected);
+        },
+      );
+    });
+
+    describe('filter', () => {
+      it('should return page 1 filtered', () => {
+        const sut = new SearchParams();
+
+        expect(sut.filter).toBe(null);
+      });
+
+      it.each([
+        [null, null],
+        [undefined, null],
+        ['', null],
+        ['test', 'test'],
+        [0, '0'],
+        [-1, '-1'],
+        [5.5, '5.5'],
+        [true, 'true'],
+        [false, 'false'],
+        [{}, '[object Object]'],
+        [1, '1'],
+        [2, '2'],
+        [25, '25'],
+      ])('filtered param %o should return %o', (filter, expected) => {
+        const sut = new SearchParams({ filter: filter as any });
+        expect(sut.filter).toBe(expected);
+      });
+    });
   });
 });
